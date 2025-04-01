eb818fcb22ab8a87000238002fc0f6153f369e2a
Zuamir Gutemberg Vieira Candido de Oliveira <zuamir.oliveira@compasso.com.br>
2025-04-01 09:43:36 -0300

    feat(MGH-001): First commit, parent module

---
+++ src/main/java/com/mygradhub/mygradhub_parent/MygradhubParentApplication.java
@@ -0,0 +1,13 @@
+package com.mygradhub.mygradhub_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygradhubParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygradhubParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mygradhub/mygradhub_parent/MygradhubParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygradhub.mygradhub_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygradhubParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
