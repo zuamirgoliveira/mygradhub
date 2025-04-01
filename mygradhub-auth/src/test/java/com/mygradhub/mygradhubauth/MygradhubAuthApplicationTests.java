1d27753686f0cbe27e993546b468432abd5bd8aa
Zuamir Gutemberg Vieira Candido de Oliveira <zuamir.oliveira@compasso.com.br>
2025-04-01 10:02:47 -0300

    feat(MGH-001): Create auth module

---
+++ mygradhub-auth/src/test/java/com/mygradhub/mygradhubauth/MygradhubAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygradhub.mygradhubauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygradhubAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
