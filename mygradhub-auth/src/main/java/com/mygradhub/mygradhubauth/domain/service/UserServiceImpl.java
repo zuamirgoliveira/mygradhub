08a9fcd6d743d7d79c6f4c60c2d57be62564100a
zuamirgoliveira <zuamirgutemberg@gmail.com>
2025-04-05 13:06:23 -0300

    refactor(MGH-008): Move exceptions to the global file

--- mygradhub-auth/src/main/java/com/mygradhub/mygradhubauth/domain/service/UserServiceImpl.java
+++ mygradhub-auth/src/main/java/com/mygradhub/mygradhubauth/domain/service/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mygradhub.mygradhubauth.domain.service;
 
+import com.mygradhub.mygradhubauth.domain.exception.BusinessRuleException;
 import com.mygradhub.mygradhubauth.domain.model.User;
 import com.mygradhub.mygradhubauth.domain.repository.UserRepository;
 import com.mygradhub.mygradhubauth.shared.AppConstants;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,7 +49,7 @@ public class UserServiceImpl implements UserService {
         return repository.findById(id)
                 .orElseThrow(() -> {
                     LOGGER.error(AppConstants.USER_NOT_FOUND_BY_ID + "{}", id);
-                    return new ResponseStatusException(HttpStatus.NOT_FOUND, AppConstants.USER_NOT_FOUND_BY_ID + id);
+                    return new EntityNotFoundException(AppConstants.USER_NOT_FOUND_BY_ID + id);
                 });
     }
 
@@ -56,11 +58,38 @@ public class UserServiceImpl implements UserService {
         User user = repository.findById(id)
                 .orElseThrow(() -> {
                     LOGGER.error(AppConstants.USER_NOT_FOUND_TO_UPDATE_BY_ID + "{}", id);
-                    return new ResponseStatusException(HttpStatus.NOT_FOUND, AppConstants.USER_NOT_FOUND_TO_UPDATE_BY_ID + id);
+                    return new EntityNotFoundException(AppConstants.USER_NOT_FOUND_TO_UPDATE_BY_ID + id);
                 });
 
         fillUpdateUser(user, updatedUser);
         return repository.save(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> {
+                    LOGGER.error(AppConstants.USER_NOT_FOUND_BY_EMAIL + "{}", email);
+                    return new EntityNotFoundException(AppConstants.USER_NOT_FOUND_BY_EMAIL + email);
+                });
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        repository.findById(id)
+                .orElseThrow(() -> {
+                    LOGGER.error(AppConstants.USER_NOT_FOUND_TO_DELETE_BY_ID + "{}", id);
+                    return new EntityNotFoundException(AppConstants.USER_NOT_FOUND_TO_DELETE_BY_ID + id);
+                });
+        repository.deleteById(id);
+        return !repository.existsById(id);
+    }
+
+    @Override
+    public void validateUsername(String username) {
+        if(repository.existsByUsername(username)) {
+            throw new BusinessRuleException(AppConstants.USERNAME_ALREADY_EXISTS);
+        }
     }
 
     private void fillUpdateUser(User user, User updatedUser) {
@@ -77,39 +106,6 @@ public class UserServiceImpl implements UserService {
             if (updatedUser.getProfilePhoto() != null) {
                 user.setProfilePhoto(updatedUser.getProfilePhoto());
             }
-        }
-    }
-
-    private void authenticate(String rawPassword, User savedUser) {
-        if (!encoder.matches(rawPassword, savedUser.getPassword())) {
-            throw new BadCredentialsException(AppConstants.UNAUTHORIZED);
-        }
-    }
-
-    @Override
-    public User findByEmail(String email) {
-        return repository.findByEmail(email)
-                .orElseThrow(() -> {
-                    LOGGER.error(AppConstants.USER_NOT_FOUND_BY_EMAIL + "{}", email);
-                    return new ResponseStatusException(HttpStatus.NOT_FOUND, AppConstants.USER_NOT_FOUND_BY_EMAIL + email);
-                });
-    }
-
-    @Override
-    public boolean deleteById(Long id) {
-        repository.findById(id)
-                .orElseThrow(() -> {
-                    LOGGER.error(AppConstants.USER_NOT_FOUND_TO_DELETE_BY_ID + "{}", id);
-                    return new ResponseStatusException(HttpStatus.NOT_FOUND, AppConstants.USER_NOT_FOUND_TO_DELETE_BY_ID + id);
-                });
-        repository.deleteById(id);
-        return !repository.existsById(id);
-    }
-
-    @Override
-    public void validateUsername(String username) {
-        if(repository.existsByUsername(username)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, AppConstants.USERNAME_ALREADY_EXISTS);
         }
     }
 
