75e44759e4e363760076fc41ddc27f5425e4f4e5
zuamirgoliveira <zuamirgutemberg@gmail.com>
2025-04-05 16:26:45 -0300

    test(MGH-008): Implemented tests, reposts, and minimum coverage ratio

--- mygradhub-auth/src/test/java/com/mygradhub/mygradhubauth/service/UserServiceImplTest.java
+++ mygradhub-auth/src/test/java/com/mygradhub/mygradhubauth/service/UserServiceImplTest.java
@@ -5,6 +5,9 @@ import com.mygradhub.mygradhubauth.domain.model.User;
 import com.mygradhub.mygradhubauth.domain.model.UserRole;
 import com.mygradhub.mygradhubauth.domain.repository.UserRepository;
 import com.mygradhub.mygradhubauth.domain.service.UserServiceImpl;
+import com.mygradhub.mygradhubauth.shared.AppConstants;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -20,6 +27,8 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
 
+    public static final String ENCRYPTED_PASSWORD = "123Hash*";
+
     @Mock
     private UserRepository userRepository;
     @Mock
@@ -27,42 +36,66 @@ public class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userServiceImpl;
 
-    @Test
-    void createUser_shouldEcryptPassword() {
-        User user = createUser();
-        String encryptedPassword = "123Hash*";
+    private User user;
+    private User savedUser;
 
-        when(encoder.encode(user.getPassword())).thenReturn(encryptedPassword);
-        when(userRepository.existsByUsername(user.getUsername())).thenReturn(false);
-        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        User savedUser = userServiceImpl.create(user);
-
-        verify(encoder, times(1)).encode("123Senha*");
-        assertEquals(encryptedPassword, savedUser.getPassword());
-    }
-
-    @Test
-    void create_WithExistingUsername_ShouldThrowException() {
-        User user = createUser();
-        String encryptedPassword = "123Hash*";
-
-        when(encoder.encode(user.getPassword())).thenReturn(encryptedPassword);
-        when(userRepository.existsByUsername(user.getUsername())).thenReturn(true);
-
-        assertThrows(BusinessRuleException.class, () -> userServiceImpl.create(user));
-
-        verify(userRepository, never()).save(any(User.class));
-    }
-
-    private static User createUser() {
-        return new User(
-                Long.parseLong("1"),
+    @BeforeEach
+    void setUp() {
+        user = new User(
                 "user123",
                 "user@gmail.com",
                 "123Senha*",
                 UserRole.USER,
                 "url...");
+        savedUser = new User(
+                Long.parseLong("1"),
+                "user123",
+                "user@gmail.com",
+                ENCRYPTED_PASSWORD,
+                UserRole.USER,
+                "url...");
     }
+
+    @Test
+    void createUserShouldEcryptPassword() {
+        when(encoder.encode(user.getPassword())).thenReturn(ENCRYPTED_PASSWORD);
+        when(userRepository.existsByUsername(user.getUsername())).thenReturn(false);
+        when(userRepository.save(user)).thenReturn(savedUser);
+
+        User createdUser = userServiceImpl.create(user);
+
+        assertThat(createdUser.getPassword()).isEqualTo(ENCRYPTED_PASSWORD);
+        assertEquals(createdUser.getId(), savedUser.getId());
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    void createUserWithExistingUsernameShouldThrowBusinessRuleException() {
+        when(encoder.encode(user.getPassword())).thenReturn(ENCRYPTED_PASSWORD);
+        when(userRepository.existsByUsername(user.getUsername())).thenReturn(true);
+
+        assertThatThrownBy(() -> userServiceImpl.create(user))
+                .isInstanceOf(BusinessRuleException.class)
+                .hasMessage(AppConstants.USERNAME_ALREADY_EXISTS);
+
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void findByIdShouldReturnUserWhenUserExists() {
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(savedUser));
+        User findedUser = userServiceImpl.findById(user.getId());
+        assertThat(findedUser).isEqualTo(savedUser);
+    }
+
+    @Test
+    void findByIdShouldThrowEntityNotFoundExceptionWhenUserDoesNotExist() {
+        when(userRepository.findById(user.getId())).thenReturn(Optional.empty());
+        assertThatThrownBy(() -> userServiceImpl.findById(user.getId()))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage(AppConstants.USER_NOT_FOUND_BY_ID + user.getId());
+    }
+
+
 
 }
